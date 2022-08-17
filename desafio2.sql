4c0b29d85e51c368995d55775a3df3c2786a7312
Bruno Negri <b.negri_@hotmail.com>
2022-08-17 16:58:15 -0300

    Requesito 2 Feito

--- desafio2.sql
+++ desafio2.sql
@@ -1,2 +1,12 @@
 -- Active: 1660763243827@@127.0.0.1@3307@SpotifyClone
-SELECT COUNT(*) AS cancoes  FROM SpotifyClone.cancoes;
+SELECT 
+
+COUNT(DISTINCT cancoes.cancao_id) AS cancoes,
+COUNT(DISTINCT artistas.artista_id) AS artistas,
+COUNT(DISTINCT albums.album_id) AS albuns
+
+FROM 
+
+SpotifyClone.cancoes AS cancoes,
+SpotifyClone.artistas AS artistas,
+SpotifyClone.albums AS albums;
